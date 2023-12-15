8e4051c2a79a7a2750e859ae66a21560e62477b5
kexV2 <B00156324@mytudublin.ie>
2023-12-15 18:54:39 +0000

    Added Project Files

---
+++ Highway Of Havoc/Assets/Scripts/Car.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    private AudioSource carAudioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get the AudioSource component attached to the car
+        carAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if the car is moving forward
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            // If the car is moving forward, play the car sound
+            if (!carAudioSource.isPlaying)
+            {
+                carAudioSource.Play();
+            }
+        }
+        else
+        {
+            // If the car is not moving forward, stop the car sound
+            carAudioSource.Stop();
+        }
+    }
+}
---
+++ Highway Of Havoc/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour
+{
+    public GameObject scoreText;
+    public int Score;
+    public AudioSource collect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("GasCan"))
+        {
+            collect.Play();
+            Score += 50;
+            scoreText.GetComponent<Text>().text = "SCORE: " + Score;
+            Destroy(other.gameObject); // Destroy the gas can instead of the current object.
+        }
+    }
+}
