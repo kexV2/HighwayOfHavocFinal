5db0c7742a9915e1c02203b303597e8a10e19afb
kexV2 <B00156324@mytudublin.ie>
2023-12-16 16:22:29 +0000

    updates

--- Highway Of Havoc/Assets/Scripts/CoinCollection.cs
+++ Highway Of Havoc/Assets/Scripts/CoinCollection.cs
@@ -2,14 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CoinCollection : MonoBehaviour
 {
-
     private int Coin = 0;
+    public int maxCoinsInCurrentLevel = 15; // Set the initial maximum coins for the first level
     public TextMeshProUGUI Score;
-
-
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,8 +16,15 @@ public class CoinCollection : MonoBehaviour
         {
             Coin++;
             Score.text = "Score: " + Coin.ToString();
-            Debug.Log(Coin);
+            Debug.Log("Coins collected: " + Coin);
             Destroy(other.gameObject);
+
+            // Check if the current level's coin count has reached its maximum
+            if (Coin == maxCoinsInCurrentLevel)
+            {
+                // Load the CompletedMenu scene
+                SceneManager.LoadScene("CompletedMenu"); // Replace "CompletedMenu" with your actual scene name
+            }
         }
     }
 }
